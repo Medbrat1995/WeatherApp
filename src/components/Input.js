4dbce5dfbeed2e2568952c96570442f71e3b2b65
Dmitriy <dmitrii.prasolov@gmail.com>
2018-04-02 12:50:05 +0300

    Add files via upload

--- src/components/Input.js
+++ src/components/Input.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import {fetchCity, changeInput} from '../actions'
 
 
+
 export default class Input extends Component {
 
 
@@ -9,21 +10,10 @@ export default class Input extends Component {
 
         const {str} = this.props;
 
-        const inputChange = (e) => {
-            e.preventDefault();
-            dispatch(changeInput(e.target.value));
-        };
-
-        const submit = (e) => {
-            e.preventDefault();
-            const city = state.input.str;
-            dispatch(fetchCity(city));
-        };
-
         return (<div>
             <form>
-                <input type='text' placeholder='Введите город' onChange={inputChange} />
-                <button type='submit' onClick={submit} />
+                <input type='text' placeholder='Введите город' onChange={this.props.onInputChange} />
+                <button type='submit' onClick={this.props.onSubmitClicked} />
             </form>
         </div>)
     }
--- src/containers/InputContainer.js
+++ src/containers/InputContainer.js
@@ -1,13 +1,23 @@
 import {connect} from 'react-redux'
 import Input from '../components/Input'
-import {changeInput} from '../actions'
+import {changeInput, fetchCity} from '../actions'
 
 
 const mapStateToProps = (state) => ({
     str: state.input.str
 });
 const mapDispatchToProps = (dispatch) => {
-    return{}
+    return{
+        onInputChange: (e) => {
+            e.preventDefault();
+            dispatch(changeInput(e.target.value));
+        },
+        onSubmitClicked: (e) => {
+            e.preventDefault();
+            const city = store.getState().input.str;
+            dispatch(fetchCity(city));
+        }
+    }
 };
 
 const InputContainer = connect(mapStateToProps, mapDispatchToProps)(Input);
